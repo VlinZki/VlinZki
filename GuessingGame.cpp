b40d1bf7476790014fc3a026f2489e3f1a403f50
VlinZki <103509010+VlinZki@users.noreply.github.com>
2022-05-18 21:17:52 +0800

    Update GuessingGame.cpp

--- GuessingGame.cpp
+++ GuessingGame.cpp
@@ -7,7 +7,7 @@ int option,mode,attemptGuess = 0,diff;
 
 // Variabel for vs computer
 const int MAX = 26;
-string alphabetGuess,alphabetAnswer;
+char alphabetGuess,alphabetAnswer;
 int numberAnswer,numberGuess;
 char alphabet[MAX] = { 'a', 'b', 'c', 'd', 'e', 'f', 'g','h', 'i', 'j', 'k', 'l', 'm', 'n','o', 'p', 'q', 'r', 's', 't', 'u','v', 'w', 'x', 'y', 'z' };
 char hints;
@@ -70,7 +70,8 @@ int main(){
             switch(diff){ // Selecting Difficulity
               case 1:
                 system("cls");
-                alphabetAnswer = alphabetAnswer + alphabet[rand() % 7];
+                attemptGuess = 0;
+                alphabetAnswer = alphabet[rand() % 7];
                 while(alphabetAnswer != alphabetGuess) {
                   system("cls");
                   cout << "         VS Computer" << endl;
@@ -81,21 +82,21 @@ int main(){
                   cout << " >> Hints? (y/n)    : ";
                   cin >> hints;
                  if(hints == 'y') {
-                  if(alphabetAnswer > "d") { cout << " The answer is above d" << endl; }
-                  if(alphabetAnswer < "d") { cout << " The answer is below d" << endl; }
+                  if(alphabetAnswer > 'd') { cout << " The answer is above d" << endl; }
+                  if(alphabetAnswer < 'd') { cout << " The answer is below d" << endl; }
                  }
-              
                  if(alphabetGuess != alphabetAnswer) { cout << " That isn't my alphabet." << endl; }
                  if(alphabetGuess == alphabetAnswer) {
                    cout << " You have guess my alphabet." << endl;
                    cout << " Number of Attempts : " << attemptGuess << endl;
                  }
-              system("pause");
-              }
-            break;
+                 system("pause");
+                }
+               break;
               case 2:
                 system("cls");
-                alphabetAnswer = alphabetAnswer + alphabet[rand() % 13];
+                attemptGuess = 0;
+                alphabetAnswer = alphabet[rand() % 13];
                 while(alphabetAnswer != alphabetGuess) {
                   system("cls");
                   cout << "         VS Computer" << endl;
@@ -106,8 +107,8 @@ int main(){
                   cout << " >> Hints? (y/n)    : ";
                   cin >> hints;
                  if(hints == 'y') {
-                  if(alphabetAnswer > "h") { cout << " The answer is above h" << endl; }
-                  if(alphabetAnswer < "h") { cout << " The answer is below h" << endl; }
+                  if(alphabetAnswer > 'h') { cout << " The answer is above h" << endl; }
+                  if(alphabetAnswer < 'h') { cout << " The answer is below h" << endl; }
                  }
               
                  if(alphabetGuess != alphabetAnswer) { cout << " That isn't my alphabet." << endl; }
@@ -115,12 +116,13 @@ int main(){
                    cout << " You have guess my alphabet." << endl;
                    cout << " Number of Attempts : " << attemptGuess << endl;
                  }
-              system("pause");
-              }
-            break;
+                 system("pause");
+                }
+                break;
               case 3:
                 system("cls");
-                alphabetAnswer = alphabetAnswer + alphabet[rand() % 19];
+                attemptGuess = 0;
+                alphabetAnswer = alphabet[rand() % 19];
                 while(alphabetAnswer != alphabetGuess) {
                   system("cls");
                   cout << "         VS Computer" << endl;
@@ -131,8 +133,8 @@ int main(){
                   cout << " >> Hints? (y/n)    : ";
                   cin >> hints;
                  if(hints == 'y') {
-                  if(alphabetAnswer > "i") { cout << " The answer is above i" << endl; }
-                  if(alphabetAnswer < "i") { cout << " The answer is below i" << endl; }
+                  if(alphabetAnswer > 'i') { cout << " The answer is above i" << endl; }
+                  if(alphabetAnswer < 'i') { cout << " The answer is below i" << endl; }
                  }
               
                  if(alphabetGuess != alphabetAnswer) { cout << " That isn't my alphabet." << endl; }
@@ -140,12 +142,13 @@ int main(){
                    cout << " You have guess my alphabet." << endl;
                    cout << " Number of Attempts : " << attemptGuess << endl;
                  }
-              system("pause");
-              }
-            break;
+                system("pause");
+                }
+                break;
               case 4:
                 system("cls");
-                alphabetAnswer = alphabetAnswer + alphabet[rand() % MAX];
+                attemptGuess = 0;
+                alphabetAnswer = alphabet[rand() % MAX];
                 while(alphabetAnswer != alphabetGuess) {
                   system("cls");
                   cout << "         VS Computer" << endl;
@@ -156,8 +159,8 @@ int main(){
                   cout << " >> Hints? (y/n)    : ";
                   cin >> hints;
                  if(hints == 'y') {
-                  if(alphabetAnswer > "m") { cout << " The answer is above m" << endl; }
-                  if(alphabetAnswer < "m") { cout << " The answer is below m" << endl; }
+                  if(alphabetAnswer > 'm') { cout << " The answer is above m" << endl; }
+                  if(alphabetAnswer < 'm') { cout << " The answer is below m" << endl; }
                  }
               
                  if(alphabetGuess != alphabetAnswer) { cout << " That isn't my alphabet." << endl; }
@@ -167,111 +170,116 @@ int main(){
                  }
               system("pause");
               }
-            break;
+                break;
             }
+          break;
           case 2: // Number
             system("cls");
             Difficulity();
             switch(diff){
               case 1: // easy difficulity
-              system("cls");
-              numberAnswer = (rand() % 10) + 1;
-                  while(numberAnswer != numberGuess){
-                    system("cls");
-                    cout << "         VS Computer" << endl;
-                    cout << "----------------------------" << endl;
-                    cout << " >> Guess my number : ";
-                    cin >> numberGuess;
-                    attemptGuess += 1;
-                    cout << " >> Hints? (y/n)    : ";
-                    cin >> hints;
-                    if(hints == 'y') {
-                      if(numberAnswer > 5 ) { cout << " The answer is above number 5" << endl; }
-                      if(numberAnswer < 5 ) { cout << " The answer is below number 5" << endl; }
-                    }
-                    
-                    if(numberGuess != numberAnswer) { cout << " That isn't my number." << endl; }
-                    if(numberGuess == numberAnswer) {
-                      cout << " You have guess my number." << endl;
-                      cout << " Number of Attempts : " << attemptGuess << endl;
-                    }
-                    system("pause");
+                system("cls");
+                attemptGuess = 0;
+                numberAnswer = (rand() % 10) + 1;
+                while(numberAnswer != numberGuess){
+                  system("cls");
+                  cout << "         VS Computer" << endl;
+                  cout << "----------------------------" << endl;
+                  cout << " >> Guess my number : ";
+                  cin >> numberGuess;
+                  attemptGuess += 1;
+                  cout << " >> Hints? (y/n)    : ";
+                  cin >> hints;
+                  if(hints == 'y') {
+                    if(numberAnswer > 5 ) { cout << " The answer is above number 5" << endl; }
+                    if(numberAnswer < 5 ) { cout << " The answer is below number 5" << endl; }
                   }
+                  
+                  if(numberGuess != numberAnswer) { cout << " That isn't my number." << endl; }
+                  if(numberGuess == numberAnswer) {
+                    cout << " You have guess my number." << endl;
+                    cout << " Number of Attempts : " << attemptGuess << endl;
+                  }
+                  system("pause");
+                }
                 break;
               case 2: // normal difficulity
-              system("cls");
-              numberAnswer = (rand() % 25) + 1;
-                  while(numberAnswer != numberGuess){
-                    system("cls");
-                    cout << "         VS Computer" << endl;
-                    cout << "----------------------------" << endl;
-                    cout << " >> Guess my number : ";
-                    cin >> numberGuess;
-                    attemptGuess += 1;
-                    cout << " >> Hints? (y/n)    : ";
-                    cin >> hints;
-                    if(hints == 'y') {
-                      if(numberAnswer > 10 ) { cout << " The answer is above number 10" << endl; }
-                      if(numberAnswer < 10 ) { cout << " The answer is below number 10" << endl; }
-                    }
-                    
-                    if(numberGuess != numberAnswer) { cout << " That isn't my number." << endl; }
-                    if(numberGuess == numberAnswer) {
-                      cout << " You have guess my number." << endl;
-                      cout << " Number of Attempts : " << attemptGuess << endl;
-                    }
-                    system("pause");
+                system("cls");
+                attemptGuess = 0;
+                numberAnswer = (rand() % 25) + 1;
+                while(numberAnswer != numberGuess){
+                  system("cls");
+                  cout << "         VS Computer" << endl;
+                  cout << "----------------------------" << endl;
+                  cout << " >> Guess my number : ";
+                  cin >> numberGuess;
+                  attemptGuess += 1;
+                  cout << " >> Hints? (y/n)    : ";
+                  cin >> hints;
+                  if(hints == 'y') {
+                    if(numberAnswer > 10 ) { cout << " The answer is above number 10" << endl; }
+                    if(numberAnswer < 10 ) { cout << " The answer is below number 10" << endl; }
                   }
+                  
+                  if(numberGuess != numberAnswer) { cout << " That isn't my number." << endl; }
+                  if(numberGuess == numberAnswer) {
+                    cout << " You have guess my number." << endl;
+                    cout << " Number of Attempts : " << attemptGuess << endl;
+                  }
+                  system("pause");
+                }
                 break;
               case 3: // hard difficulity
-              system("cls");
-              numberAnswer = (rand() % 50) + 1;
-                  while(numberAnswer != numberGuess){
-                    system("cls");
-                    cout << "         VS Computer" << endl;
-                    cout << "----------------------------" << endl;
-                    cout << " >> Guess my number : ";
-                    cin >> numberGuess;
-                    attemptGuess += 1;
-                    cout << " >> Hints? (y/n)    : ";
-                    cin >> hints;
-                    if(hints == 'y') {
-                      if(numberAnswer > 25 ) { cout << " The answer is above number 25" << endl; }
-                      if(numberAnswer < 25 ) { cout << " The answer is below number 25" << endl; }
-                    }
-                    
-                    if(numberGuess != numberAnswer) { cout << " That isn't my number." << endl; }
-                    if(numberGuess == numberAnswer) {
-                      cout << " You have guess my number." << endl;
-                      cout << " Number of Attempts : " << attemptGuess << endl;
-                    }
-                    system("pause");
+                system("cls");
+                attemptGuess = 0;
+                numberAnswer = (rand() % 50) + 1;
+                while(numberAnswer != numberGuess){
+                  system("cls");
+                  cout << "         VS Computer" << endl;
+                  cout << "----------------------------" << endl;
+                  cout << " >> Guess my number : ";
+                  cin >> numberGuess;
+                  attemptGuess += 1;
+                  cout << " >> Hints? (y/n)    : ";
+                  cin >> hints;
+                  if(hints == 'y') {
+                    if(numberAnswer > 25 ) { cout << " The answer is above number 25" << endl; }
+                    if(numberAnswer < 25 ) { cout << " The answer is below number 25" << endl; }
                   }
+                  
+                  if(numberGuess != numberAnswer) { cout << " That isn't my number." << endl; }
+                  if(numberGuess == numberAnswer) {
+                    cout << " You have guess my number." << endl;
+                    cout << " Number of Attempts : " << attemptGuess << endl;
+                  }
+                  system("pause");
+                }
                 break;
               case 4: // insane difficulity
-              system("cls");
+                system("cls");
+                attemptGuess = 0;
                 numberAnswer = (rand() % 100) + 1;
-                  while(numberAnswer != numberGuess){
-                    system("cls");
-                    cout << "         VS Computer" << endl;
-                    cout << "----------------------------" << endl;
-                    cout << " >> Guess my number : ";
-                    cin >> numberGuess;
-                    attemptGuess += 1;
-                    cout << " >> Hints? (y/n)    : ";
-                    cin >> hints;
-                    if(hints == 'y') {
-                      if(numberAnswer > 50 ) { cout << " The answer is above number 50" << endl; }
-                      if(numberAnswer < 50 ) { cout << " The answer is below number 50" << endl; }
-                    }
-                    
-                    if(numberGuess != numberAnswer) { cout << " That isn't my number." << endl; }
-                    if(numberGuess == numberAnswer) {
-                      cout << " You have guess my number." << endl;
-                      cout << " Number of Attempts : " << attemptGuess << endl;
-                    }
-                    system("pause");
+                while(numberAnswer != numberGuess){
+                  system("cls");
+                  cout << "         VS Computer" << endl;
+                  cout << "----------------------------" << endl;
+                  cout << " >> Guess my number : ";
+                  cin >> numberGuess;
+                  attemptGuess += 1;
+                  cout << " >> Hints? (y/n)    : ";
+                  cin >> hints;
+                  if(hints == 'y') {
+                    if(numberAnswer > 50 ) { cout << " The answer is above number 50" << endl; }
+                    if(numberAnswer < 50 ) { cout << " The answer is below number 50" << endl; }
                   }
+                  
+                  if(numberGuess != numberAnswer) { cout << " That isn't my number." << endl; }
+                  if(numberGuess == numberAnswer) {
+                    cout << " You have guess my number." << endl;
+                    cout << " Number of Attempts : " << attemptGuess << endl;
+                  }
+                  system("pause");
+                }
                 break;
             }
           break;
@@ -283,6 +291,7 @@ int main(){
         switch(mode){ // Selecting mode
           case 1: // Word (a-z)
             system("cls");
+            attemptGuess = 0;
             cout << "Enter your alphabet : ";
             cin >> playerAlphabetAnswer;
             system("cls");
@@ -291,29 +300,40 @@ int main(){
               cout << "           VS MODE" << endl;
               cout << "----------------------------" << endl;
               cout << " >> Guess my alphabet : ";
-              cin >> playerAlphabetAnswer;
+              cin >> playerAlphabetGuess;
+              attemptGuess += 1;
               if(playerAlphabetGuess != playerAlphabetAnswer) { cout << " That isn't the answer." << endl; }
               if(playerAlphabetGuess == playerAlphabetAnswer) {
                 cout << " You have guess the answer." << endl;
                 cout << " Number of Attempts : " << attemptGuess << endl;
               }
+              system("pause");
             }
             break;
           case 2: // Number (1-100)
             system("cls");
+            attemptGuess = 0;
             cout << "Enter your number : ";
             cin >> playerNumberAnswer;
-            system("cls");
-            while(playerNumberAnswer != playerNumberGuess){
+            if(playerNumberAnswer > 100 || playerNumberAnswer < 0) {
+              cout << "That number is out of range." << endl;
+              system("pause");
+            }
+            else{
               system("cls");
-              cout << "           VS MODE" << endl;
-              cout << "----------------------------" << endl;
-              cout << " >> Guess my number : ";
-              cin >> playerNumberGuess;
-              if(playerNumberGuess != playerNumberAnswer) { cout << " That isn't the number." << endl; }
-              if(playerNumberGuess == playerNumberAnswer) {
-                cout << " You have guess the answer." << endl;
-                cout << " Number of Attempts : " << attemptGuess << endl;
+              while(playerNumberAnswer != playerNumberGuess){
+                system("cls");
+                cout << "           VS MODE" << endl;
+                cout << "----------------------------" << endl;
+                cout << " >> Guess my number : ";
+                cin >> playerNumberGuess;
+                attemptGuess += 1;
+                if(playerNumberGuess != playerNumberAnswer) { cout << " That isn't the number." << endl; }
+                if(playerNumberGuess == playerNumberAnswer) {
+                  cout << " You have guess the answer." << endl;
+                  cout << " Number of Attempts : " << attemptGuess << endl;
+                }
+                system("pause");
               }
             }
             break;
@@ -321,15 +341,17 @@ int main(){
         break;
       case 3: // Rules
         system("cls");
-        cout << "               RULES" << endl;
-        cout << "---------------------------------------" << endl;
-        cout << "| 1. Guess a alphabet or number       |" << endl;
-        cout << "| 2. No Cheating for 2 player         |" << endl;
-        cout << "| 3. For Hints in VS mode the player  |" << endl;
-        cout << "| who is enter the number have to give|" << endl;
-        cout << "| hints like low or high              |" << endl;
-        cout << "| 4. Have Fun :D                      |" << endl;
-        cout << "---------------------------------------" << endl;
+        cout << "                  RULES                  " << endl;
+        cout << "-----------------------------------------" << endl;
+        cout << "| 1. Guess a alphabet or number         |" << endl;
+        cout << "| 2. No Cheating for 2 player           |" << endl;
+        cout << "| 3. For Hints in VS mode the player    |" << endl;
+        cout << "| who is enter the number have to give  |" << endl;
+        cout << "| hints like low or high                |" << endl;
+        cout << "| 4. For VS Player, the answer must be  |" << endl;
+        cout << "| above 0 and below 100                 |" << endl;
+        cout << "| 5. Have Fun :D                        |" << endl;
+        cout << "-----------------------------------------" << endl;
         system("pause");
         break;
     }
